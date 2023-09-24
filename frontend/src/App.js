b8a190b8401f3d5186c18f480ce69dc1abe65b21
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-24 20:45:17 +0100

    Added basic react/manifest.json files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { LandingPage } from './pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='h-96 w-80 bg-gray-900'>
+          <LandingPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
