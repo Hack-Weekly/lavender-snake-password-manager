d559b812fd052b030ce3751d45d80192cd43c60d
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-26 20:54:09 +0100

    Added home page

---
+++ frontend/src/components/AddButton.jsx
@@ -0,0 +1,7 @@
+export const AddButton = () => {
+    return (
+        <button className="bg-purple-500 w-11/12 h-10 rounded-md text-lg font-bold transition duration-300 ease-in-out hover:bg-purple-600 hover:shadow-md hover:text-white">
+            Add
+        </button>
+    );
+};
